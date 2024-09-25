14482a45e7e5062253aed167cc772e43f4a13854
Debdwaipayan <debdwaipayan02@gmail.com>
2024-09-25 23:48:10 +0530

    power of a number

---
+++ Numbers/power.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,po;
+    cout<<"Enter the number and the power it needs to be raised to"<<endl;
+    cin>>n>>po;
+    cout<<"The power is "<<pow(n,po);
+}
